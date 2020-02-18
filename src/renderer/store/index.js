3b942712a6d043b0e49b61322d6103f0ab99d74a
Victor Martinez <knoopx@gmail.com>
2020-02-18 16:38:58 +0100

    FIX: Catch ticker update failures

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,16 +1,14 @@
-import { sum, sortBy } from 'lodash'
-import { types, flow } from 'mobx-state-tree'
-import {
-  autorun, observe, untracked, values,
-} from 'mobx'
-import { now } from 'mobx-utils'
+import { sum, sortBy } from "lodash"
+import { types, flow } from "mobx-state-tree"
+import { autorun, observe, untracked, values } from "mobx"
+import { now } from "mobx-utils"
+import { ipcRenderer } from "electron"
 
-import { ipcRenderer } from 'electron'
-import { formatNumber } from 'support/formatting'
+import { formatNumber } from "support/formatting"
+import baseCurrencies from "data/baseCurrencies"
+import periods from "data/periods"
 
-import baseCurrencies from 'data/baseCurrencies'
-import periods from 'data/periods'
-import Ticker from './Ticker'
+import Ticker from "./Ticker"
 
 function parseTicker({
   rank,
@@ -32,7 +30,7 @@ function parseTicker({
     percent_change_24h: parseFloat(percent_change_24h),
     percent_change_7d: parseFloat(percent_change_7d),
     ...baseCurrencies
-      .map(x => x.toLocaleLowerCase())
+      .map((x) => x.toLocaleLowerCase())
       .reduce(
         (result, c) => ({
           ...result,
@@ -50,18 +48,18 @@ function parseTicker({
 }
 
 export default types
-  .model('Store', {
+  .model("Store", {
     lastUpdate: types.maybeNull(types.number),
-    query: types.optional(types.string, ''),
-    baseCurrency: types.optional(types.string, 'USD'),
+    query: types.optional(types.string, ""),
+    baseCurrency: types.optional(types.string, "USD"),
     tickers: types.optional(types.map(Ticker), {}),
     // tickerLimit: types.optional(types.number, 25),
     refreshInterval: types.optional(types.number, 30000),
     isFetching: types.optional(types.boolean, false),
     showOnlyHolding: types.optional(types.boolean, false),
-    period: types.optional(types.enumeration(periods), '1W'),
+    period: types.optional(types.enumeration(periods), "1W"),
   })
-  .views(self => ({
+  .views((self) => ({
     get matchingHoldingsTickers() {
       if (self.showOnlyHolding) {
         return self.holdingTickers
@@ -73,24 +71,24 @@ export default types
     },
     get matchingTickers() {
       return sortBy(
-        self.matchingHoldingsTickers.filter(t => t.matches(self.query)),
-        'rank',
+        self.matchingHoldingsTickers.filter((t) => t.matches(self.query)),
+        "rank",
       )
     },
     get portfolioValue() {
-      return sum(self.holdingTickers.map(c => c.holdings * c.price))
+      return sum(self.holdingTickers.map((c) => c.holdings * c.price))
     },
     get portfolioChange1h() {
-      const x = self.holdingTickers.filter(c => c.percent_change_1h)
-      return sum(x.map(c => c.percent_change_1h)) / x.length
+      const x = self.holdingTickers.filter((c) => c.percent_change_1h)
+      return sum(x.map((c) => c.percent_change_1h)) / x.length
     },
     get portfolioChange1d() {
-      const x = self.holdingTickers.filter(c => c.percent_change_24h)
-      return sum(x.map(c => c.percent_change_24h)) / x.length
+      const x = self.holdingTickers.filter((c) => c.percent_change_24h)
+      return sum(x.map((c) => c.percent_change_24h)) / x.length
     },
     get portfolioChange7d() {
-      const x = self.holdingTickers.filter(c => c.percent_change_7d)
-      return sum(x.map(c => c.percent_change_7d)) / x.length
+      const x = self.holdingTickers.filter((c) => c.percent_change_7d)
+      return sum(x.map((c) => c.percent_change_7d)) / x.length
     },
     get nextUpdateAt() {
       return self.lastUpdate + self.refreshInterval
@@ -119,19 +117,19 @@ export default types
         disposables.push(
           autorun(() => {
             ipcRenderer.send(
-              'set-title',
+              "set-title",
               `${formatNumber(self.portfolioValue)} ${self.baseCurrency}`,
             )
           }),
         )
         disposables.push(
-          observe(self, 'portfolioValue', ({ oldValue, newValue }) => {
+          observe(self, "portfolioValue", ({ oldValue, newValue }) => {
             if (newValue > oldValue) {
-              self.setIcon('▲', 'green')
+              self.setIcon("▲", "green")
             } else if (newValue < oldValue) {
-              self.setIcon('▼', 'red')
+              self.setIcon("▼", "red")
             } else {
-              self.setIcon('▶', 'black')
+              self.setIcon("▶", "black")
             }
           }),
         )
@@ -148,30 +146,32 @@ export default types
       setIcon(char, color) {
         const size = 22
         const fontSize = 18
-        const canvas = document.createElement('canvas')
-        const context = canvas.getContext('2d')
+        const canvas = document.createElement("canvas")
+        const context = canvas.getContext("2d")
         canvas.height = size
         canvas.width = size
         context.fillStyle = color
         context.font = `${fontSize}px Arial`
         const { width } = context.measureText(char)
         context.fillText(char, size / 2 - width / 2, fontSize)
-        ipcRenderer.send('set-icon', canvas.toDataURL())
+        ipcRenderer.send("set-icon", canvas.toDataURL())
       },
       beforeDestroy() {
         disposables.forEach((dispose) => {
           dispose()
         })
       },
-      fetchTickers: flow(function* () {
+      fetchTickers: flow(function*() {
         self.setIsFetching(true)
         try {
-          const url = new URL('https://api.coinmarketcap.com/v1/ticker/')
-          url.searchParams.append('convert', self.baseCurrency)
+          const url = new URL("https://api.coinmarketcap.com/v1/ticker/")
+          url.searchParams.append("convert", self.baseCurrency)
           // url.searchParams.append('limit', self.tickerLimit)
-          url.searchParams.append('fresh', Date.now())
+          url.searchParams.append("fresh", Date.now())
           const response = yield fetch(url)
           self.setTickers(yield response.json())
+        } catch (err) {
+          console.error("Failed to fetch tickers")
         } finally {
           self.setIsFetching(false)
           self.touch()
