e910c950ef12660b8c2be84727884209d6ae5f99
Victor Martinez <knoopx@gmail.com>
2020-02-18 16:23:34 +0100

    FIX: Casename

--- src/renderer/components/input.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import classNames from 'classnames'
-
-const Input = ({ className, ...props }) => (
-  <input className={classNames("flex flex-auto appearance-none outline-none color-inherit bg-transparent", className)} {...props} />
-)
-
-export default Input
